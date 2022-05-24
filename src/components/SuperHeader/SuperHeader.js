35057e93d9936858a7ac930dcff0bb185a5a20c6
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-05-24 16:48:24 +0200

    Style super header.

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,15 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  top: 0;
+  gap: 24px;
+  padding: 8px 16px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
